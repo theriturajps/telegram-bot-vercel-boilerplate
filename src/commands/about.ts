91a0a32a2e8c99bb5bc71f14f17095f9027f9055
The RituRajPS <theriturajps@gmail.com>
2023-07-27 14:09:00 +0530

    Update about.ts

--- src/commands/about.ts
+++ src/commands/about.ts
@@ -6,9 +6,24 @@ import { author, name, version } from '../../package.json';
 const debug = createDebug('bot:about_command');
 
 const about = () => async (ctx: Context) => {
-  const message = `*${name} ${version}*\n${author}`;
+  const message = `This is Made By A Human\n*${name} ${version}*\n\n${author}`;
+  const keyboard = {
+    inline_keyboard: [
+      [
+        {
+          text: 'click here',
+          url: 'https://t.me/riturajps'
+        }
+      ]
+    ]
+  };
+
   debug(`Triggered "about" command with message \n${message}`);
-  await ctx.replyWithMarkdownV2(message, { parse_mode: 'Markdown' });
+  await ctx.replyWithMarkdownV2(message, {
+    parse_mode: 'Markdown',
+    reply_markup: keyboard,
+    disable_web_page_preview: true // Disables preview for the message itself
+  });
 };
 
 export { about };
