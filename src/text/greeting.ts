8149de105b8eda3d94ba9532932bc9b81402dcf0
The RituRajPS <theriturajps@gmail.com>
2023-07-27 14:08:32 +0530

    Update greeting.ts

--- src/text/greeting.ts
+++ src/text/greeting.ts
@@ -6,16 +6,21 @@ const debug = createDebug('bot:greeting_text');
 const replyToMessage = (ctx: Context, messageId: number, string: string) =>
   ctx.reply(string, {
     reply_to_message_id: messageId,
+    parse_mode: 'Markdown', // Set parse_mode to Markdown
   });
 
 const greeting = () => async (ctx: Context) => {
   debug('Triggered "greeting" text command');
 
   const messageId = ctx.message?.message_id;
-  const userName = `${ctx.message?.from.first_name} ${ctx.message?.from.last_name}`;
+  const userId = ctx.message?.from.id;
+  const userName = `${ctx.message?.from.first_name}`;
 
-  if (messageId) {
-    await replyToMessage(ctx, messageId, `Hello, ${userName}!`);
+  if ((ctx.message as any)?.text === '/start') { // Use type assertion to access the 'text' property
+    if (messageId && userId) {
+      const helloMessage = `Hello, [${userName}](tg://user?id=${userId})!, welcome to the bot\n\nBy @RituRajPS`; // Use Markdown syntax for hyperlink
+      await replyToMessage(ctx, messageId, helloMessage);
+    }
   }
 };
 
